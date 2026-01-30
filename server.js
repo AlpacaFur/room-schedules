948e08485d72257b4e6f5f6490bb74456a4960a1
Luke Taylor <luke@bigring.org>
2026-01-30 15:42:01 -0500

    Spring 26

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware'
 const PORT = 9000
 const server = express()
 
-const rooms = JSON.parse(fs.readFileSync("./room-data/fall25.json"))
+const rooms = JSON.parse(fs.readFileSync("./room-data/spring26-jan12.json"))
 const room_names = Object.keys(rooms)
 const ROOMS_JSON = {}
 room_names.forEach((name) => {
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const CURRENT_SEMESTER_CODE = "202610"
-export const CURRENT_YEAR = 25
-export const CURRENT_SEMESTER = "Fall"
+export const CURRENT_SEMESTER_CODE = "202630"
+export const CURRENT_YEAR = 26
+export const CURRENT_SEMESTER = "Spring"
